14b48e39f9d8379084cb9916d63b62f3ba8ad91e
sai jagan <yetukurisaijagan@gmail.com>
2025-09-20 23:48:55 +0530

    v-1--_1.1_1.1

--- frontend/shell-app-angular/src/app/app-routing.module.ts
+++ frontend/shell-app-angular/src/app/app-routing.module.ts
@@ -1,29 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { loadRemoteModule } from '@angular-architects/module-federation';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'pos', pathMatch: 'full' },
-  {
-    path: 'pos',
-    loadChildren: () => loadRemoteModule({
-      type: 'module',
-      remoteEntry: 'http://localhost:4201/remoteEntry.js',
-      exposedModule: './PosModule',
-    }).then((m) => m.PosModule),
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => loadRemoteModule({
-      type: 'module',
-      remoteEntry: 'http://localhost:4202/remoteEntry.js',
-      exposedModule: './DashboardModule',
-    }).then((m) => m.DashboardModule),
-  },
-];
-
+const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
