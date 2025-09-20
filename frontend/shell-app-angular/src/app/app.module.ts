b80422d0f94b3a375507cca3c4f7c3019dd17dd4
sai jagan <yetukurisaijagan@gmail.com>
2025-09-20 23:18:07 +0530

    v-1--_1.1_

--- frontend/shell-app-angular/src/app/app.module.ts
+++ frontend/shell-app-angular/src/app/app.module.ts
@@ -6,16 +6,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatIconModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }" > src/app/app.module.ts
+
+# Create src/app/app-routing.module.ts
+echo "import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }" > src/app/app-routing.module.ts
+
+# Create src/app/app.component.ts
+echo "import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: []
+})
+export class AppComponent {
+  title = 'shell-app-angular';
+}
